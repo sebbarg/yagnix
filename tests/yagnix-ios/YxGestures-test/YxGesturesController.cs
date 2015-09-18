54c3e4e04ac100ad0ab6aa95a415e86b582c757d
Sebastian Bargmann <sebbarg@gmail.com>
2015-09-18 12:39:24 +0200

    added TouchDownGestureRecognizer

---
+++ tests/yagnix-ios/YxGestures-test/YxGesturesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UIKit;
+using Yagnix;
+using Yagnix.YxGestures;
+
+namespace YxGesturestest
+{
+  public class YxGesturesController : UIViewController
+  {
+    public YxGesturesController()
+    {
+      Console.WriteLine($"Version {UIDevice.CurrentDevice.SystemVersion}");
+    }
+
+    public override void ViewDidLoad()
+    {
+      base.ViewDidLoad();
+
+      var view = View.Add<UIView>();
+      view.FitToGuides(this, 100);
+      view.BackgroundColor = UIColor.Blue;
+      view.UserInteractionEnabled = true;
+
+
+//      var onTouch = new Action( () => {
+//        Console.WriteLine("Touch!");
+//      });
+    
+      var onTouch = new Action<TouchDownGestureRecognizer>( (gesture) => {
+        var pt = gesture.LocationInView(gesture.View);
+        Console.WriteLine($"X: {pt.X}");
+      });
+
+      var downGesture = new TouchDownGestureRecognizer(onTouch);
+
+      view.AddGestureRecognizer(downGesture);
+    }
+  }
+}
+
---
+++ yagnix-ios/YxGestures/TouchDownGestureRecognizer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UIKit;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+
+namespace Yagnix.YxGestures
+{
+  public class TouchDownGestureRecognizer : UIGestureRecognizer
+  {
+    private readonly Action<TouchDownGestureRecognizer> _action;
+
+    public TouchDownGestureRecognizer(Action action) : base(action)
+    {
+    }
+
+    //
+
+    public TouchDownGestureRecognizer(Action<TouchDownGestureRecognizer> action)
+    {
+      _action = action;
+
+      AddTarget(() => {
+        if (State == UIGestureRecognizerState.Recognized)
+        {
+          if (_action != null)
+          {
+            _action(this);
+          }
+        }
+      });
+    }
+
+    //
+
+    public override void TouchesBegan(NSSet touches, UIEvent evt)
+    {
+      base.TouchesBegan(touches, evt);
+      if ( State == UIGestureRecognizerState.Possible )
+        State = UIGestureRecognizerState.Recognized;
+    }
+
+    //
+
+    public override void TouchesCancelled(NSSet touches, UIEvent evt)
+    {
+      base.TouchesCancelled(touches, evt);
+      State = UIGestureRecognizerState.Failed;
+    }
+
+    //
+
+    public override void TouchesMoved(NSSet touches, UIEvent evt)
+    {
+      base.TouchesMoved(touches, evt);
+      State = UIGestureRecognizerState.Failed;
+    }
+
+    //
+
+    public override void TouchesEnded(NSSet touches, UIEvent evt)
+    {
+      base.TouchesEnded(touches, evt);
+      State = UIGestureRecognizerState.Failed;
+    }
+
+    //
+  }
+}
+
